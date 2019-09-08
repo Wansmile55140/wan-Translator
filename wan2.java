2f3bb5b23f6091c349e7a65b6c0f780a31931b2e
Wansmile55140 <42538817+Wansmile55140@users.noreply.github.com>
2019-09-08 15:21:50 +0700

    Add files via upload

---
+++ wan2.java
@@ -0,0 +1,22 @@
+public class wan2 {
+  public static void main(String args[]) {
+
+boolean a1=true;
+byte a2=30;
+short a3=-15;
+double a4=14.15;
+long a5= 6;
+float a6 = 14.548f;
+int a7 =34;
+char a8 ='wan';
+
+System.out.println(a1);
+System.out.println(a2);
+System.out.println(a3);
+System.out.println(a4); 
+System.out.println(a5);
+System.out.println(a6);
+System.out.println(a7);
+System.out.println(a8);
+}
+}
